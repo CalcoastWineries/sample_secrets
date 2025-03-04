24697f534ad7e0498c296d29e001969a2db14aad
CalcoastWineries <35215270+CalcoastWineries@users.noreply.github.com>
2025-03-04 16:31:57 +0800

    my secret leak 20250304

--- postgres_model.js
+++ postgres_model.js
@@ -5,3 +5,9 @@ var pg_user="root";
 var pg_pass="sup3rstr0ngpass1ForGG2025";
 
 var mongo_uri = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority2025";
+
+[default]
+aws_access_key_id = AKIAQYLPMN5HHHFPZAM2
+aws_secret_access_key = 1tUm636uS1yOEcfP5pvfqJ/ml36mF7AkyHsEU0IUe9Fc2260a86a5fADc8e8FE4D798D29d4eD5Cd8394E33E64532DAAbEE3AAbd7b27aeCab6
+output = json
+region = us-east-2
