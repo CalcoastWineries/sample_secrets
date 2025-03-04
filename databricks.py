efeac998efb9b2ebefe5b133a679886837a9e9bb
CalcoastWineries <35215270+CalcoastWineries@users.noreply.github.com>
2025-03-04 20:28:54 +0800

    my secret leak 20250304 2028

--- databricks.py
+++ databricks.py
@@ -3,5 +3,5 @@ import os
 
 with sql.connect(server_hostname = os.getenv("DATABRICKS_SERVER_HOSTNAME"),
                  http_path       = os.getenv("DATABRICKS_HTTP_PATH"),
-                 access_token    = os.getenv("DATABRICKS_TOKEN")) as connection:
+                 access_token    = os.getenv("dbc-43a5252d-fe61.cloud.databricks.com")) as connection:
 # ...
