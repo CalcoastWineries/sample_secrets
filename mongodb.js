3d753c3c2ed3b7728845538840ee354e7a0a327f
Ubuntu <ubuntu@ip-172-31-20-198.us-east-2.compute.internal>
2025-03-04 15:06:03 +0000

    my secert leak Tue Mar  4 15:06:03 UTC 2025

--- mongodb.js
+++ mongodb.js
@@ -1,1 +1,1 @@
-mongodb+srv://<db_username>:<db_password>@cluster-so.3xmab.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-So
+mongodb+srv://mongodb:TmGMMVS7r8BflgD2@cluster-so.3xmab.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-So
