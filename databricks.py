d80c697044adfa38a7c5bfb82db98d8e6576046c
CalcoastWineries <35215270+CalcoastWineries@users.noreply.github.com>
2025-03-04 20:06:04 +0800

    init

---
+++ databricks.py
@@ -0,0 +1,7 @@
+from databricks import sql
+import os
+
+with sql.connect(server_hostname = os.getenv("DATABRICKS_SERVER_HOSTNAME"),
+                 http_path       = os.getenv("DATABRICKS_HTTP_PATH"),
+                 access_token    = os.getenv("DATABRICKS_TOKEN")) as connection:
+# ...
